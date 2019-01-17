6f3dca160be9c1da9af5b49e134addf638990e54
Wojciech Pater <wpater@itersive.com>
2019-01-17 13:48:19 +0100

    Backup MongoDB
    Scripts for backup and restore MongoDB. Works on docker too.

---
+++ Backups/MongoDB/backup.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+
+# Print usage
+scriptName=$0
+usage() {
+  echo -n "Usage:
+  ${scriptName} host [OPTION]
+
+  Examples:
+  ${scriptName} host                        creates not encrypted archive with backup_date.tar.gz name from given host
+  ${scriptName} host -o backup              creates not encrypted backup.tar.gz archive from given host
+  ${scriptName} host -o backup -e           creates encrypted backup.tar.gz archive from given host and asks for passphrase
+  ${scriptName} host -o backup -eg          creates encrypted backup.tar.gz archive from given host with generated passphrase
+  ${scriptName} host -o backup -ep pass     creates encrypted backup.tar.gz archive from given host with given passphrase
+  ${scriptName} host -o backup -c           creates not encrypted backup.tar.gz archive from given 'host' container
+
+  Options:
+  -c, --container           Create backup from Docker container with given host name
+  -e, --encrypt             Archive will be encrypted
+  -g, --generate            Passphrase will be generated and printed at the end
+  -h, --help                Prints this message
+  -p, --passphrase string   Passphrase to use during encryption
+  -o, --output string       Name of archive. Otherwise name will be backup_date.tar.gz
+
+"
+}
+
+OPTIONS=ceghp:o:
+LONGOPTS=container,encrypt,generate,help,passphrase:,output:
+
+! PARSED=$(getopt --options=$OPTIONS --longoptions=$LONGOPTS --name "$0" -- "$@")
+if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
+    exit 2
+fi
+eval set -- "$PARSED"
+
+container=n encrypt=n generate=n output=backup_$(date +%Y%m%d).tar.gz passphrase=-
+
+while true; do
+    case "$1" in
+        -c|--container)
+            container=y
+            shift
+            ;;
+        -e|--encrypt)
+            encrypt=y
+            shift
+            ;;
+        -g|--generate)
+            generate=y
+            # generate random 32 character alphanumeric string
+            passphrase=$(cat /dev/urandom \
+              | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+            shift
+            ;;
+        -h|--help)
+            usage
+            exit 1
+            ;;
+        -p|--passphrase)
+            passphrase="$2"
+            shift 2
+            ;;
+        -o|--output)
+            output="$2.tar.gz"
+            shift 2
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            echo "Error"
+            exit 3
+            ;;
+    esac
+done
+
+# handle non-option arguments
+if [[ $# -ne 1 ]]; then
+    echo "$0: A host is required."
+    exit 4
+fi
+
+host=$1
+temp_output=temp.tar.gz
+backup_dir=backup
+
+mkdir $backup_dir
+
+if [ $container = "y" ]; then
+    net=temp_backup_network
+    docker network create $net &>/dev/null
+    docker network connect $net $host
+    docker run --net $net -v $backup_dir:/backup mongo bash -c 'mongodump --out /backup --host '$host':27017' &>/dev/null
+    docker network disconnect $net $host
+    docker network rm $net &>/dev/null
+else
+    mongodump --out $backup_dir --host $host:27017
+fi
+
+tar -czC $backup_dir . >$temp_output
+
+# handle encryption
+if [ $encrypt = "y" ]; then
+    if [ $passphrase = "-" ]; then
+      openssl enc -e -aes256 -md sha256 -in $temp_output -out $output
+    else
+      openssl enc -e -aes256 -md sha256 -in $temp_output -k $passphrase -out $output
+      if [ $generate = "y" ]; then
+        echo "Archive encrypted with:" $passphrase
+      fi
+    fi
+    rm $temp_output
+else
+    mv $temp_output $output
+fi
+
+rm -rf $backup_dir
---
+++ Backups/MongoDB/restore.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+
+# Print usage
+scriptName=$0
+usage() {
+  echo -n "Usage:
+  ${scriptName} host backup [OPTION]
+
+  Examples:
+  ${scriptName} host backup                 restores not encrypted backup on given host
+  ${scriptName} host backup -e              restores encrypted archive in given volume and asks for passphrase
+  ${scriptName} host backup -ep pass        restores encrypted archive in given volume with given passphrase
+
+  Options:
+  -c, --container           Restores backup in Docker container with given host name
+  -e, --encrypted           Archive will be decrypted
+  -h, --help                Prints this message
+  -p, --passphrase string   Passphrase to use during encryption
+  -v, --volume string       Name of volume
+
+"
+}
+
+OPTIONS=cehp:v:
+LONGOPTS=container,encrypted,help,passphrase:,volume:
+
+# -use ! and PIPESTATUS to get exit code with errexit set
+# -temporarily store output to be able to check for errors
+# -activate quoting/enhanced mode (e.g. by writing out “--options”)
+# -pass arguments only via   -- "$@"   to separate them correctly
+! PARSED=$(getopt --options=$OPTIONS --longoptions=$LONGOPTS --name "$0" -- "$@")
+if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
+    # e.g. return value is 1
+    #  then getopt has complained about wrong arguments to stdout
+    exit 2
+fi
+eval set -- "$PARSED"
+
+container=n encrypted=n volume=- passphrase=-
+
+while true; do
+    case "$1" in
+        -c|--container)
+            container=y
+            shift
+            ;;
+        -e|--encrypted)
+            encrypted=y
+            shift
+            ;;
+        -h|--help)
+            usage
+            exit 1
+            ;;
+        -p|--passphrase)
+            passphrase="$2"
+            shift 2
+            ;;
+        -v|--volume)
+            volume="$2"
+            shift 2
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            echo "Error"
+            exit 3
+            ;;
+    esac
+done
+
+# handle non-option arguments
+if [[ $# -ne 2 ]]; then
+    echo "$0: Backup file and host are required."
+    exit 4
+fi
+
+archive=$1
+host=$2
+temp_output=temp.tar.gz
+backup_dir=backup
+
+mkdir $backup_dir
+
+# handle decryption
+if [ $encrypted = "y" ]; then
+    if [ $passphrase = "-" ]; then
+      openssl enc -d -aes256 -md sha256 -in $archive -out $temp_output
+    else
+      openssl enc -d -aes256 -md sha256 -in $archive -k $passphrase -out $temp_output
+    fi
+    mv $temp_output $archive
+fi
+
+tar -xzC $backup_dir <$archive
+
+if [ $container = "y" ]; then
+    net=temp_backup_network
+    docker network create $net &>/dev/null
+    docker network connect $net $host
+    docker run --net $net -v $backup_dir:/backup mongo bash -c 'mongorestore /backup --host '$host':27017' &>/dev/null
+    docker network disconnect $net $host
+    docker network rm $net &>/dev/null
+else
+    mongorestore $backup_dir --host $host:27017
+fi
+
+rm -rf $backup_dir
