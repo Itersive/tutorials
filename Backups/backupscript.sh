11aa143f066ef610105b24add552ebbe96f6d023
Aleksander Jasiak <ajasiak@vps519542.ovh.net>
2019-04-07 03:09:35 +0200

    Backup script for postgresql and mongodb
    Modified output, added postgres backup solution

--- Backups/backupscript.sh
+++ Backups/backupscript.sh
@@ -25,6 +25,7 @@ runContBackup(){
     case $vendor in
         postgres)
             echo -e "Starting postgres backup for $host"
+            docker exec backupcontainer bash -c 'pg_basebackup -F t -z -h '$host' -p '$port' -U backupuser -w -X stream -D /backup/'$host'_$(date +%Y%m%d%H%M%S)'
             ;;
         mongodb)
             echo -e "Starting mongodb backup for $host"
